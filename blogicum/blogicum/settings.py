15ccbb1b74fe8abbb607a98f712426bb7192c2ba
Konstantin Makeev <kostia1500@gmail.com>
2024-06-02 23:06:23 +0300

    add standart templates (base, footer, header)

--- blogicum/blogicum/settings.py
+++ blogicum/blogicum/settings.py
@@ -53,10 +53,13 @@ MIDDLEWARE = [
 
 ROOT_URLCONF = 'blogicum.urls'
 
+
+TEMPLATES_DIR = BASE_DIR / 'templates'
+
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [TEMPLATES_DIR],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -129,3 +132,5 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 STATICFILES_DIRS = [
     BASE_DIR / 'static_dev',
 ]
+
+
