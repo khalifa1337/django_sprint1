a536586de50007854f9f81732dcdf32982ce6362
Konstantin Makeev <kostia1500@gmail.com>
2024-06-03 12:29:41 +0300

    Complete all basic tasks

--- blogicum/blog/urls.py
+++ blogicum/blog/urls.py
@@ -9,7 +9,7 @@ urlpatterns = [
         'posts/<int:id>/',
         views.post_detail,
         name='post_detail'
-        ),
+    ),
     path(
         'category/<slug:category_slug>/',
         views.category_posts,
--- blogicum/pages/views.py
+++ blogicum/pages/views.py
@@ -2,8 +2,12 @@ from django.shortcuts import render
 
 # Create your views here.
 
-def about():
-    pass
 
-def rules():
-    pass
+def about(request):
+    template = 'pages/about.html'
+    return render(request, template)
+
+
+def rules(request):
+    template = 'pages/rules.html'
+    return render(request, template)
